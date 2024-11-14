59cf762fe71890f288696cf5abb64c43b48d533a
JhonBsA <brandon.serrano585@gmail.com>
2024-11-14 03:03:23 -0600

    Se agrega el movimiento libre al juego con WAS, Mouse y limitacion para que no se salga de la vista

---
+++ TowerDefense/Assets/Scripts/CameraController.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private bool doMovement = true;
+
+    public float panSpeed = 30f;
+    public float panBorderThickness = 10f;
+
+    public float scrollSpeed = 5f;
+    public float minY = 10f;
+    public float maxY = 30f;
+
+    public float minX = -10f;
+    public float maxX = 10f;
+    public float minZ = -10f;
+    public float maxZ = -5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            doMovement = !doMovement;
+        }
+
+        if (!doMovement)
+        {
+            return;
+        }
+
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos += Vector3.forward * panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos += Vector3.back * panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos += Vector3.right * panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            pos += Vector3.left * panSpeed * Time.deltaTime;
+        }
+
+        // Movimiento de la cámara en el eje Y (scroll)
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        // Aplicar límites en los ejes X y Z
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.z = Mathf.Clamp(pos.z, minZ, maxZ);
+
+        transform.position = pos;
+    }
+}
